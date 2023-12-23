b24df8b780126ead1d878e511d26dfa12bab0b90
Joshua Blew <joshua@normalfinance.io>
2023-12-23 12:13:29 -0600

    branding

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'LP Helper',
+  appId: 'io.normalfinance.lp-helper',
+  appName: 'Normal LP Helper',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
